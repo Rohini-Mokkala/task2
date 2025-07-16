e4209c0bcf5960958341397bd6e5fd5c440f0a17
Rohini-Mokkala <mokkalarohini.btech@gmail.com>
2025-07-16 12:30:00 +0000

    first-commit

--- resource.tf
+++ resource.tf
@@ -1,56 +1,29 @@
 provider "azurerm" {
-  features {}
+features {}
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = "example-rg"
+resource "azurerm_resource_group" "terra" {
+  name     = "terra"
   location = "East US"
 }
-
-resource "azurerm_virtual_network" "vnet" {
-  name                = "example-vnet"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+ 
+resource "azurerm_virtual_network" "terra_vnet" {
+    name                = "terra-vnet"
+    address_space       = ["10.0.0.0/16"]
+    location            = azurerm_resource_group.terra.location
+    resource_group_name = azurerm_resource_group.terra.name
 }
-
-resource "azurerm_subnet" "subnet" {
-  name                 = "example-subnet"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
+ 
+resource "azurerm_subnet" "subnet1" {
+    name                 = "subnet1"
+    resource_group_name  = azurerm_resource_group.terra.name
+    virtual_network_name = azurerm_virtual_network.terra_vnet.name
+    address_prefixes     = ["10.0.1.0/24"]
 }
-
-resource "azurerm_network_interface" "nic" {
-  name                = "example-nic"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
-
-resource "azurerm_windows_virtual_machine" "vm" {
-  name                  = "example-vm"
-  resource_group_name   = azurerm_resource_group.rg.name
-  location              = azurerm_resource_group.rg.location
-  size                  = "Standard_DS1_v2"
-  admin_username        = "azureuser"
-  admin_password        = "YourP@ssword123!"
-  network_interface_ids = [azurerm_network_interface.nic.id]
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_reference {
-    publisher = "MicrosoftWindowsServer"
-    offer     = "WindowsServer"
-    sku       = "2019-Datacenter"
-    version   = "latest"
-  }
+ 
+resource "azurerm_subnet" "subnet2" {
+    name                 = "subnet2"
+    resource_group_name  = azurerm_resource_group.terra.name
+    virtual_network_name = azurerm_virtual_network.terra_vnet.name
+    address_prefixes     = ["10.0.2.0/24"]
 }
